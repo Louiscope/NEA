4d0ddd79b6a14b09b89d75b3ed2b6cbc01b20ae9
Louiscope <100944677+Louiscope@users.noreply.github.com>
2023-03-27 16:43:37 +0100

    Update FinishLevel.cs

--- FinishLevel.cs
+++ FinishLevel.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 
 public class FinishLevel : MonoBehaviour
 {
-    // Finds Player position to reset upon level completion
+    
     public Vector3 PlayerPos;
     public Vector3 StartPos = new Vector3(0, 1, 0);
     public GameObject Player;
     
-    // Contains Script calls for RandomGen.cs and SetDifficulty.cs -- Contains values for weighting to be used in SetDifficulty.cs
+    // Contains values for weighting to be used in SetDifficulty.cs
     RandomGen rg;
     SetDifficulty sd;
     public float Points;
@@ -31,7 +31,6 @@ public class FinishLevel : MonoBehaviour
     [SerializeField] public int i = 0;
     [SerializeField] public double S;
     
-    // Mainly Setter of Values such as Points and deaths -- Finds GameObjects with the scripts of RandomGen.cs and SetDifficulty.cs attached
     void Start()
     {
         Points = 0;
@@ -41,33 +40,32 @@ public class FinishLevel : MonoBehaviour
         rg = GameObject.FindGameObjectWithTag("Rand").GetComponent<RandomGen>();
         sd = GameObject.FindGameObjectWithTag("SetDiff").GetComponent<SetDifficulty>();
     }
-    // Finds current player position for spawning
     void Update()
     {
         PlayerPos = new Vector3(Player.transform.position.x - 432, 1, Player.transform.position.z - 432);
-        // Checks if the finish is not already spawned and the amount of kills is at least 5
+        
         if ((!Spawned) & (EnemyKillCount >= 5))
         {
             SpawnFinish();
         }
     }
-    // Used for debugging / initial setting of difficulty when game starts
+    // Used for debugging
     public void SetScores(int val)
     {
         Scores[0] = val * 20; Scores[1] = val * 20; Scores[2] = val * 20;
     }
-    // Spawns the finish tile
+    
     void SpawnFinish()
     {
         GameObject Finish = Instantiate(FinishPrefab, FinishPos, Quaternion.identity);
         Spawned = true;
     }
-    // Increases EnemyKillCount value by 1 upon and enemy being killed, called from EnemyAiTutorial.cs
+    
     public void AddKillCount()
     {
         EnemyKillCount++;
     }
-    // Resets the kill count back to 0 and the bool checker for the finish tile to false, called in RandomGen.cs
+    
     public void Reset()
     {
         PrevDeathEnvironment = DeathEnvironment;
@@ -82,39 +80,39 @@ public class FinishLevel : MonoBehaviour
     {
         return Math.Max(PrevDeathEnemy, PrevDeathEnvironment);
     }
-    // Once the player enters a trigger (The finish tile)
+    
     public void OnTriggerEnter(Collider other)
     {
-        // Checks that the trigger is the finish
+        
         if (other.tag == "Fin")
         {
-            // Applies calculation with values and stores to current Scores location between 0 and 2
+           
             Scores[i % 3] = sd.Calc(Points, Time.time - StartTime, DeathEnemy, DeathEnvironment);
             i++;
             S = sd.avg(Scores);
-            // Sets new start time from the time the last level was completed
+            
             StartTime = Time.time;
-            // Sets a new seed for the random generation, as it is milliseconds there is an infinitesmal chance it is the same as another if there were infinite tiles
+            
             seed = System.DateTime.Now.Millisecond;
             Destroy("Tile");
-            // Moves player to start position
+            
             Player.transform.position = new Vector3(PlayerPos.x, PlayerPos.y, PlayerPos.z);
-            // Set up and calling of Gen in RandomGem.cs
+            
             UnityEngine.Random.InitState(seed);
             rg.Gen();
         }
     }
     
-    // Uses the Reciprocal Exponential calculation found in SetDifficulty.cs for RandomGen.cs to use in the tilesets selection -- Refer to documentation for more information on the difficulty system
+    
     public int GetDifficulty()
     {
         return sd.f(S);
     }
 
-    // Adds to death count, uses bool to determine type of death as only 2 types, Enemy and environment
-    public void AddDeath(bool Z)
+    
+    public void AddDeath(bool DeathType)
     {
-        if (Z)
+        if (DeathType)
         {
             DeathEnemy++;
         }
@@ -124,7 +122,7 @@ public class FinishLevel : MonoBehaviour
         }
         Player.transform.position = new Vector3(StartPos.x, 1, StartPos.z);
     }
-    // Increases points by 5000 until 100000 which is the cap -- Refer to the documentation for information on the points system
+    
     public void AddPoints()
     {
         Points += 5000;
@@ -133,7 +131,7 @@ public class FinishLevel : MonoBehaviour
             Points = 100000;
         }
     }
-    // Removes all game objects of a certain name and removes the finish tile
+    
     public void Destroy(string tag)
     {
         GameObject[] DestroyObjs = GameObject.FindGameObjectsWithTag(tag);
